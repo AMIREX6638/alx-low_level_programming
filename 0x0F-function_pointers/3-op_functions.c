3ab5f2b21e6327214dd8f8ede94a646ff2eb4cb4
AMIREX6638 <rayaami0966@gmail.com>
2023-01-18 18:43:14 -0800

    Task 0

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculate the sum
+ * @a: first
+ * @b: second
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculate difference
+ * @a: firsy
+ * @b: second
+ *
+ * Return: diffrence of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates multiply
+ * @a: first
+ * @b: second
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates the division
+ * @a: first
+ * @b: second
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculates
+ * @a: first
+ * @b: second
+ *
+ * Return: remainder
+ */
+{
+	return (a % b);
+}
+
